e55c2991e0f46bb3716a7db648645a57aa9fc3d6
Cotbert2 <mateojgdev@gmail.com>
2024-11-05 00:42:17 -0500

    put service subject

--- src/backend/controllers/actions.controller.js
+++ src/backend/controllers/actions.controller.js
@@ -50,5 +50,59 @@ export const createSubject = async (req, res) => {
     }
 };
 
-export const updateSubjectInfoById = async (req, res) => {};
-export const deleteSubjectInfoById = async (req, res) => {};
+export const updateSubjectInfoById = async (req, res) => {
+    const {
+        fullname,
+        bithdate,
+        deathdate,
+        birthplace,
+        spousesId,
+        fatherId,
+        motherId,
+    } = req.body;
+
+    try {
+        const newSubject ={
+            fullname,
+            bithdate,
+            deathdate,
+            birthplace,
+            spousesId,
+            fatherId,
+            motherId,
+        };
+
+        const savedSubject = await Subject.findByIdAndUpdate(req.params.id, newSubject);
+        console.log(`Subject ${fullname} created successfully`);
+
+        //http resolve
+        res.statusMessage = 'Subject updated successfully';
+        res.status(200);
+        res.json(savedSubject);
+        res.end();
+    } catch (error) {
+
+        console.error(`Error: ${error.message}`);
+        res.statusMessage = 'Subject creation failed';
+        res.status(500).end();
+
+    }
+};
+
+
+export const deleteSubjectInfoById = async (req, res) => {
+    const idToDelete = req.params.id;
+
+    try {
+        await Subject.deleteOne({ _id: idToDelete });
+        res.statusMessage = 'Subject deleted successfully';
+
+        //http resolve
+        res.status(200);
+        res.end();
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        res.statusMessage = 'Subject deletion failed';
+        res.status(500).end();
+    }
+};
